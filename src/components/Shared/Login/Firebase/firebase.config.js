6591500250d841a4aff4fb5e4c685a60f907251b
Rahim-sarker <mhrahim08@gmail.com>
2022-01-29 22:02:14 +0600

    Complete my project

---
+++ src/components/Shared/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCht9pCWZljVwzK3wJTRY7yrJe5I902EE4",
+  authDomain: "crolla-car.firebaseapp.com",
+  projectId: "crolla-car",
+  storageBucket: "crolla-car.appspot.com",
+  messagingSenderId: "835771192963",
+  appId: "1:835771192963:web:34a0aa4174cdec1c77edb6"
+};
+export default firebaseConfig;
